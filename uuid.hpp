7344f006dba1c42f58376390a60421ee92306c85
Bakkah <abu.cudi@googlemail.com>
2013-12-29 19:29:32 +0100

    Update uuid.hpp
    algorithm changed, added missing version and variant information in uuid

--- uuid.hpp
+++ uuid.hpp
@@ -5,57 +5,50 @@
 #include <random>
 #include <chrono>
 #include <array>
+#include <sstream>
+#include <iomanip>
 
 namespace UUID {
 
 class UUID final {
 public:
   UUID()
-        : uuid_({
-                '0','0','0','0','0','0','0','0','-',
-                '0','0','0','0','-',
-                '0','0','0','0','-',
-                '0','0','0','0','-',
-                '0','0','0','0','0','0','0','0','0','0','0','0',
-        }) {
+        : bytes_({0}) {
 
         }
         ~UUID() {
 
         }
         std::string to_string() const {
-                return uuid_.data();
+        	std::ostringstream oss;
+        	for(auto i = 0; i < 16; ++i) {
+        		oss << std::setw(2) << std::setfill('0') << std::hex << ((std::uint32_t)bytes_[i]);
+        		if(i == 3 or i == 5 or i == 7 or i == 9)
+        		        oss << "-";
+        	}
+            return oss.str();
         }
-        static UUID new_uuid() {
+        static UUID random() {
                 UUID uuid;
                 auto seed = std::chrono::system_clock::now().time_since_epoch().count();
                 std::default_random_engine random_engine(seed);
-                std::uniform_int_distribution<int8_t> index(0,15);
+                std::uniform_int_distribution<uint8_t> random_bits(0,255);
+                for(int i = 0; i < 16; ++i) {
+                	uuid.bytes_[i] = random_bits(random_engine);
+                }
 
-
-                auto random_char = [&random_engine,&index]() {
-
-                	return hex_chars[index(random_engine)];
-                };
-                uuid.uuid_ = {
-                		random_char(), random_char(), random_char(), random_char(),
-                                random_char(), random_char(), random_char(), random_char(), '-',
-                                random_char(), random_char(), random_char(), random_char(), '-',
-                                random_char(), random_char(), random_char(), random_char(), '-',
-                                random_char(), random_char(), random_char(), random_char(), '-',
-                                random_char(), random_char(), random_char(), random_char(),
-                                random_char(), random_char(), random_char(), random_char(),
-                                random_char(), random_char(), random_char(), random_char(),
-                                '\0'
-                };
-                return uuid;
+                uuid.bytes_[6] &= 0x0F; // clear version
+                uuid.bytes_[6] |= 0x40; // set version
+                uuid.bytes_[8] &= 0x3F; // clear variant
+                uuid.bytes_[8] |= 0x80; // set variant
+           return uuid;
+        }
+        std::array<unsigned char,16> bytes() const {
+        	return bytes_;
         }
 private:
-        static constexpr const char *hex_chars = "0123456789abcdef";
-        std::array<char,37> uuid_;
-
+        std::array<unsigned char,16> bytes_;
 };
-
 }
 
 
